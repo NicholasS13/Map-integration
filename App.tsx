08d7bb107cefb04594af0ad41a0ad8df1037e2c6
NicholasS13 <37513914+NicholasS13@users.noreply.github.com>
2020-07-11 20:13:31 -0400

    added app .tsx
    Tao this is the file ive been working on

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps'
+export default class App extends Component {
+  render() {
+    return (
+      <MapView style={{ ...StyleSheet.absoluteFillObject }}> 
+      </MapView>
+      
+    );
+  }
+}
